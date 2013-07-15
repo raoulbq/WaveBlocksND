bc8d721e1a52022c716ab730c1040359777c4549
Raoul Bourquin <raoulb@student.ethz.ch>
2013-07-15 12:07:50 +0200

    Improve delegate handling in inner product hierarchies

--- src/WaveBlocksND/InnerProduct.py
+++ src/WaveBlocksND/InnerProduct.py
@@ -7,7 +7,7 @@ methods. InnerProducts are classes that really can compute things
 like inner products (brakets) etc.
 
 @author: R. Bourquin
-@copyright: Copyright (C) 2011, 2012 R. Bourquin
+@copyright: Copyright (C) 2011, 2012, 2013 R. Bourquin
 @license: Modified BSD License
 """
 
@@ -31,7 +31,7 @@ class InnerProduct(object):
 
 
     def get_description(self):
-        r"""Return a description of this quadrature object.
+        r"""Return a description of this inner product object.
         A description is a ``dict`` containing all key-value pairs
         necessary to reconstruct the current instance. A description
         never contains any data.
@@ -39,24 +39,22 @@ class InnerProduct(object):
         raise NotImplementedError("'InnerProduct' is an abstract interface.")
 
 
-    # TODO: Rename 'quadrature', the delegate can be something other than a classical quadrature.
-    def set_quadrature(self, quad):
+    def set_delegate(self, delegate):
         r"""Set the :py:class:`Quadrature` subclass instance used for quadrature.
 
-        :param quad: The new :py:class:`Quadrature` instance.
+        :param delegate: The new :py:class:`Quadrature` instance.
         """
         # TODO: Allow a list of quads, one quad for each component of Psi
-        self._quad = quad
+        self._delegate = delegate
 
 
-    # TODO: Rename 'quadrature', the delegate can be something other than a classical quadrature.
-    def get_quadrature(self):
+    def get_delegate(self):
         r"""Return the :py:class:`Quadrature` subclass instance
         used for evaluation of this inner product.
 
         :return: The current instance of the quadrature.
         """
-        return self._quad
+        return self._delegate
 
 
     def quadrature(self):
