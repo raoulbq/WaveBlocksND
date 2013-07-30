43f8b7bc3370e2347379a3bbbd1f484f4efa603d
Raoul Bourquin <raoulb@student.ethz.ch>
2013-07-30 16:29:47 +0200

    New explicit Henon-Heiles potential
    The potential is taken from the paper:
    
    @article{davis_heller,
      author = {Michael J. Davis and Eric J. Heller},
      title = {Semiclassical Gaussian basis set method for molecular vibrational wave functions},
      publisher = {AIP},
      year = {1979},
      journal = {The Journal of Chemical Physics},
      volume = {71},
      number = {8},
      pages = {3383-3395},
    }

--- src/WaveBlocksND/PotentialLibrary.py
+++ src/WaveBlocksND/PotentialLibrary.py
@@ -192,6 +192,12 @@ eckart_bn["variables"] = ["x", "y"]
 eckart_bn["potential"] = "v0*cosh(a*x)**(-2) + k/2*(1-sigma*exp(-lam*x**2)) * y**2"
 eckart_bn["defaults"] = {"v0":"0.425", "a":"1.3624", "k":"0.06784", "sigma":"0.5", "lam":"0.25"}
 
+# Henon-Heiles 2D
+henon_heiles = {}
+henon_heiles["variables"] = ["x", "y"]
+henon_heiles["potential"] = "1/2*a*(x**2 + y**2) + b*(x**2*y - y**3/3.0)"
+henon_heiles["defaults"] = {"a":"1", "b":"1/2"}
+
 #######################################################################
 # Potentials in three dimensions and with one energy level (D=3, N=1) #
 #######################################################################
