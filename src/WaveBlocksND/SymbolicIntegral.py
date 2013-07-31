8c9be3d1a61ad921bf80900584be12913213ccf8
Raoul Bourquin <raoulb@student.ethz.ch>
2013-07-31 17:58:03 +0200

    Adapt SymbolicIntegral to use compatibility checks

--- src/WaveBlocksND/SymbolicIntegral.py
+++ src/WaveBlocksND/SymbolicIntegral.py
@@ -16,11 +16,12 @@ from scipy.special import binom
 #from scipy.special.orthogonal import eval_hermite
 
 from Quadrature import Quadrature
+from InnerProductCompatibility import InnerProductCompatibility
 
 __all__ = ["SymbolicIntegral"]
 
 
-class SymbolicIntegral(Quadrature):
+class SymbolicIntegral(Quadrature, InnerProductCompatibility):
     r"""
     """
 
@@ -43,6 +44,10 @@ class SymbolicIntegral(Quadrature):
         d = {}
         d["type"] = "SymbolicIntegral"
         return d
+
+
+    def get_kind(self):
+        return ("homogeneous", "inhomogeneous")
 
 
     def initialize_packet(self, pacbra, packet=None):
