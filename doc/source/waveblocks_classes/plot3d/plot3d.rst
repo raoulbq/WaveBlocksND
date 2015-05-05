8e5099cd15fbbfeefa030f8da2a51a6914b5ad2d
Raoul Bourquin <raoul.bourquin@sam.math.ethz.ch>
2015-05-05 18:50:31 +0200

    Removed non-doc stuff

--- doc/source/waveblocks_classes/plot3d/plot3d.rst
+++
@@ -1,9 +0,0 @@
-Plot 3D
-=======
-
-This plot function requires mayavi.
-
-``surfcf``
-----------
-
-.. autofunction:: WaveBlocksND.Plot3D.surfcf
--- src/WaveBlocksND/Plot3D/__init__.py
+++
@@ -1,8 +0,0 @@
-"""The WaveBlocks Project
-
-@author: R. Bourquin
-@copyright: Copyright (C) 2012 R. Bourquin
-@license: Modified BSD License
-"""
-
-from surfcf import surfcf
